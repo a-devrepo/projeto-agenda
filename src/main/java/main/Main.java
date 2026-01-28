d35b511d8a3c96e95d67b6eac11535038c31e1d1
alison <adevpr11@gmail.com>
2026-01-28 14:33:58 -0300

    (main): adiciona classe principal

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import controllers.CompromissoController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var controller = new CompromissoController();
+        controller.gerarCompromisso();
+    }
+}
